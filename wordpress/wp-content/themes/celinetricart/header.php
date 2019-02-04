647d3511cef09670298818704b72d682fd4eec46
tombiliby <tombiliby@gmail.com>
2019-02-04 19:33:56 +0100

    all in one plugin and title desc site

--- wordpress/wp-content/themes/celinetricart/header.php
+++ wordpress/wp-content/themes/celinetricart/header.php
@@ -5,7 +5,8 @@
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 		
-		<title>ORCHESTRA</title>
+		<title><?php echo get_bloginfo( 'name' ); ?></title>
+        <meta name="description" content="<?php echo get_bloginfo( 'description' ); ?>">
 
 		<!-- Fonts -->
 		<link href="https://fonts.googleapis.com/css?family=Roboto:300,400,700" rel="stylesheet">
